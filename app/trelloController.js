c3c8cdd725d778e2d1fc006b7182488ab24a2297
mat <m.hemmerle@adhoc-gti.com>
2018-02-18 23:10:20 +0100

    Demarrer saisie et annulation saisie

--- app/trelloController.js
+++ app/trelloController.js
@@ -2,6 +2,22 @@
 
 angular.module('app').controller('trelloController', function ($scope, trelloService, glpiService) {
     $scope.cartes = []
+    var modeAJoutCarte = false
+    $scope.saisieVisible = function saisieVisible () {
+        return modeAJoutCarte
+    }
+
+    $scope.ajoutCarteVisible = function ajoutCarteVisible () {
+        return !modeAJoutCarte
+    }
+
+    $scope.demarrerSaisie = function demarrerSaisie() {
+        modeAJoutCarte = true
+    }
+
+    $scope.annulerSaisie = function annulerSaisie() {
+        modeAJoutCarte = false
+    }
 
     var tablink
     var url = window.location.toString()
